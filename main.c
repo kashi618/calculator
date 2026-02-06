1210b76b4343bd0621c2025f5615af1a92d672c1
kashi <nialj618@gmail.com>
2026-02-06 21:38:34 +0000

    Window sizing and messing around

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 static void activate(GApplication *app, gpointer *user_data) {
     GtkWidget *win;
     
-    win = gtk_window_new();
+    win = gtk_application_window_new(GTK_APPLICATION(app));
     
+    // Window Properties
+    gtk_window_set_title(GTK_WINDOW(win), "GTK Calculator");
+    gtk_window_set_default_size(GTK_WINDOW(win), 600, 800);
+    gtk_window_set_resizable(GTK_WINDOW(win), FALSE);
     gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
+    
+    // Show window
     gtk_window_present(GTK_WINDOW(win));
 }
 
